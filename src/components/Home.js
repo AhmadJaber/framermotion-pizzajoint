c5ab2396b126ef75e56be0837f342786f2c75d21
AhmadJaber <ash1kahmadjaber@gmail.com>
2022-02-23 00:15:25 +0600

    container classname modifed for tailwind container

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,17 +3,13 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div className="home container">
-      <h2>
-        Welcome to Pizza Joint
-      </h2>
-      <Link to="/base">
-        <button>
-          Create Your Pizza
-        </button>
+    <div className='home container-manual'>
+      <h2>Welcome to Pizza Joint</h2>
+      <Link to='/base'>
+        <button>Create Your Pizza</button>
       </Link>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
--- src/components/Toppings.js
+++ src/components/Toppings.js
@@ -2,31 +2,34 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Toppings = ({ addTopping, pizza }) => {
-  let toppings = ['mushrooms', 'peppers', 'onions', 'olives', 'extra cheese', 'tomatoes'];
+  let toppings = [
+    'mushrooms',
+    'peppers',
+    'onions',
+    'olives',
+    'extra cheese',
+    'tomatoes',
+  ];
 
   return (
-    <div className="toppings container">
-      
+    <div className='toppings container-manual'>
       <h3>Step 2: Choose Toppings</h3>
       <ul>
-        {toppings.map(topping => {
+        {toppings.map((topping) => {
           let spanClass = pizza.toppings.includes(topping) ? 'active' : '';
           return (
             <li key={topping} onClick={() => addTopping(topping)}>
-              <span className={spanClass}>{ topping }</span>
+              <span className={spanClass}>{topping}</span>
             </li>
-          )
+          );
         })}
       </ul>
 
-      <Link to="/order">
-        <button>
-          Order
-        </button>
+      <Link to='/order'>
+        <button>Order</button>
       </Link>
-
     </div>
-  )
-}
+  );
+};
 
 export default Toppings;
